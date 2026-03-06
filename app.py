ccb63c61311fe3eceed3aeefa2031aead37ae97d
kukuku100424-bit <kukuku100424@gmail.com>
2026-03-07 00:01:29 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -519,7 +519,7 @@ const CATEGORY_LIST = [
 
 const map = L.map("map", { zoomControl:true }).setView([34.85, 126.90], 9);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
   maxZoom: 19,
   attribution: "&copy; OpenStreetMap"
 }).addTo(map);
